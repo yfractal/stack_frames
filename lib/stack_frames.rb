b93244e96e2e7708b80ef630fb286abb3ef99d14
Mike Yang <yfractal@gmail.com>
2024-07-22 09:46:38 +0800

    feat: move analyzer from scripts to lib

--- lib/stack_frames.rb
+++ lib/stack_frames.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'stack_frames/version'
+require 'stack_frames/analyzer'
 require 'stack_frames/stack_frames'
 
 StackFrames::Frame.singleton_class.class_eval do
